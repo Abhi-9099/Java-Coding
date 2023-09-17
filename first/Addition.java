ccef4ed50c38fe3628c326e23003e79a882129ab
Abhi-9099 <akchaudharysp65@gmail.com>
2023-09-17 12:00:16 +0530

    first java file

---
+++ first/Addition.java
@@ -0,0 +1,14 @@
+package first;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=20,b=30;
+		int c=a+b;
+		System.out.println("Enter the first value=" + a);
+		System.out.println("Enter the second value=" + b);
+		System.out.println("Enter the sum value=" + c);
+
+	}
+
+}
